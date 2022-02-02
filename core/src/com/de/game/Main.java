ae4f617dfe2c112d55dbd5cec19a79c9d12aac01
LTW2014 <joshua.schwager@schwager-online.de>
2022-02-02 11:51:23 +0100

    test2

--- core/src/com/de/game/Main.java
+++ core/src/com/de/game/Main.java
@@ -11,7 +11,7 @@ public class Main extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("2badlogic.jpg");
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
