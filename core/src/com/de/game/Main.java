10f3a18f0764cb3e7a818f4b4f7ecc4e523c57d7
Jibryl <germangames@gmx.de>
2022-02-04 07:59:39 +0100

    v.a.004

--- core/src/com/de/game/Main.java
+++ core/src/com/de/game/Main.java
@@ -3,12 +3,11 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.graphics.GL20;
 
 public class Main extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	Gameworld world1 = new Gameworld();
+	//Gameworld world1 = new Gameworld();
 	
 	@Override
 	public void create () {
