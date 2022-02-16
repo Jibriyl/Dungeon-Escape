8648f260ca4a7daaef121f14de440740343a61e2
Jibriyl <germangames@gmx.de>
2022-02-16 12:36:14 +0100

    v.a.010

--- core/src/com/de/game/MAIN_GAME.java
+++ core/src/com/de/game/MAIN_GAME.java
@@ -4,10 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
-import com.badlogic.gdx.physics.box2d.World;
 
 public class MAIN_GAME extends ScreenAdapter{
 
